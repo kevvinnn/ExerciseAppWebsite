56b5eaa0ee3e866d41baca3d16c3b2b8747ae6fc
Kevin Fields <fieldskevin001@gmail.com>
2021-03-10 20:23:53 -0500

    share

--- client1/src/views/Share.vue
+++ client1/src/views/Share.vue
@@ -6,28 +6,38 @@
   <div class = "column">
   <div class = "share">
     <form class = "box">
+      <form @submit.prevent="addPost">
       <h1>Share Progress</h1>
       <div class="control">
-        <input class="input" type="text" placeholder="Name">
+        <input class="input" type="text" placeholder="Name" v-model="newPost.user.name">
       </div>
       <div class="control">
-       <input class="input" type="text" placeholder="@Username">
+       <input class="input" type="text" placeholder="@Username" v-model="newPost.user.username">
       </div>
       <div class="control">
-       <input class="input" type="text" placeholder="Image Source">
+       <input class="input" type="text" placeholder="Image Source" v-model="newPost.imgsrc">
       </div>
       <div class="control">
-       <textarea class="textarea" placeholder="Caption"></textarea>
+       <textarea class="textarea" placeholder="Caption" v-model="newPost.caption"></textarea>
       </div>
       <div class="field">
        <p class="control">
         <button class="button is-primary">
           Post
         </button>
-      </p>
+       </p>
     </div>
+      </form>
     </form>
-  </div>
+     <div class="content-item" v-for="(post, i) in posts" :key="i">
+                <post-layout :post="post" />
+              </div>
+          </div>
+          <div class="column is-one-quarter">
+              <div class="content-item">
+                <post-layout :post="newPost" />
+              </div>
+          </div>
   </div>
 </div>
 
@@ -36,10 +46,35 @@
 </template>
 
 <script>
-import FriendFinder from '../components/FriendFinder.vue'
+import FriendFinder from '../components/FriendFinder.vue';
+import PostLayout from '../components/PostLayout.vue';
 
 export default {
-  components: { FriendFinder },
+  data: ()=> ({
+    newPost: {
+      user: { }
+    },
+    posts: [
+      {
+        imgsrc: "https://bulma.io/images/placeholders/1280x960.png",
+        caption: "",
+        time: Date(),
+        user: {
+          name: "",
+          username: "",
+          profpic: "https://bulma.io/images/placeholders/96x96.png",
+        }
+      }
+    ]
+  }),
+  components: { FriendFinder, PostLayout },
+
+  methods: {
+    addPost(){
+            this.posts.unshift(this.newPost);
+            this.newPost = { user: {} }
+        }
+  }
 
 }
 </script>
